07cfb15550c9921a134d0961583ca54204d56fb5
Pavel Nichita <nichitapavel@live.com>
2012-09-24 00:12:10 +0100

    Utilizando pagina externa!

--- Practica_1_rpc/test.rb
+++ Practica_1_rpc/test.rb
@@ -5,9 +5,13 @@ require 'erb'
 # and set up an array of viable moves that a player (and the
 # computer) can perform
 before do
-  @defeat = { rock: :scissors, paper: :rock, scissors: :paper}
+  @defeat = { :rock => :scissors, :paper => :rock, :scissors => :paper}
   @throws = @defeat.keys
 end
+
+#get '/' do
+    #erb: index
+#end
 
 get '/throw/:type' do
   # the params hash stores querystring and form data
@@ -19,25 +23,10 @@ get '/throw/:type' do
 
   if @player_throw == @computer_throw
     @answer = "There is a tie"
-    erb :index
   elsif @player_throw == @defeat[@computer_throw]
     @answer = "Computer wins; #{@computer_throw} defeats #{@player_throw}"
-    erb :index
   else
     @answer = "Well done. #{@player_throw} beats #{@computer_throw}"
-    erb :index
   end
+  #erb :results
 end
-__END__
-
-@@index
-<html>
-<head>
-<title>Rock Paper Scissors</title>
-</head>
-<body>
-<h2> Computer chooses: <%= @computer_throw %> </h2>
-<h2> You choose: <%= @player_throw %> </h2>
-<h1> <%= @answer %> </h1>
-</body>
-</html>
